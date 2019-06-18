24500c88c9bbc16a97c757ecb2e400f6f42a4631
liuyonghua <liuyonghua1996@gmail.com>
2019-06-18 18:10:08 +0800

    乱七八糟

---
+++ src/views/system/dept/Dept.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "Dept"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/system/dept/DeptEdit.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "DeptEdit"
+    }
+</script>
+
+<style scoped>
+
+</style>
